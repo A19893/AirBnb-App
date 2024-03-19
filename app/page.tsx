5941e9ba17801a11fa3601a1d24a7fad65638b70
A19893 <yasharora2678@gmail.com>
2024-03-19 17:28:02 +0530

    Added listing component page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { Listing } from "@prisma/client";
 import getCurrentUser from "./actions/getCurrentUser";
 import getListings from "./actions/getListings";
 import ClientOnly from "./components/ClientOnly";
@@ -21,7 +22,7 @@ export default async function Home() {
       <Container>
         <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
             {
-              listings.map((listing: any)=> {
+              listings.map((listing: Listing)=> {
                 return(
                   <ListingCard key={listing.id} data={listing} currentUser={currentUser}/>
                 )
